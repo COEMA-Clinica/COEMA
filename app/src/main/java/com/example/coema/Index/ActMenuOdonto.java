a4f7792432a42139586f6b4428f2aa7af2bb4928
AValentinoGPereda <angel_garcia3@usmp.pe>
2023-10-11 16:31:39 -0500

    Odonto vista

---
+++ app/src/main/java/com/example/coema/Index/ActMenuOdonto.java
@@ -0,0 +1,20 @@
+package com.example.coema.Index;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.coema.R;
+
+public class ActMenuOdonto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.odontologo_principal);
+    }
+
+
+}
