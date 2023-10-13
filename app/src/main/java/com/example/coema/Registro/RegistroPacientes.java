2f064600de47602eef22f4e4f79be0d52f5f933f
pierog <pgarcia@usmpvirtual.edu.pe>
2023-10-13 11:32:03 -0500

    Actualización ya registra usuario y permite conectar, relacion tabla usuario y tabla paciente

--- app/src/main/java/com/example/coema/Registro/RegistroPacientes.java
+++ app/src/main/java/com/example/coema/Registro/RegistroPacientes.java
@@ -1,15 +1,10 @@
 package com.example.coema.Registro;
 
-import static com.example.coema.R.id.txtFechaR;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.os.StrictMode;
-import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -20,28 +15,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.coema.Conection.DatabaseConnection;
-import com.example.coema.Fragments.FragmentSelectorFecha;
-import com.example.coema.Login.IniciarSesion;
+import com.example.coema.Index.Principal;
 import com.example.coema.R;
+
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.List;
 
 public class RegistroPacientes extends AppCompatActivity {
 
-    //Declaracion de variables
     EditText edtNom, edtApe, edtCor, edtCon, edtDni, edtTel, edtFec;
     TextView txtYaTienesCuenta;
     Spinner spnSexR;
-
-    int aActual, dActual, mActual;
-    int fecA, fecM, fecD;
-
-    String spnSexVal[]={"Hombre", "Mujer"};
     Button btnRegistrar;
 
     @Override
@@ -50,119 +38,141 @@ public class RegistroPacientes extends AppCompatActivity {
         setContentView(R.layout.registrar_paciente);
 
         asignarReferencias();
-
     }
 
-    //Declaracion de id's
     private void asignarReferencias() {
-
         edtCor = findViewById(R.id.txtEmailR);
         edtApe = findViewById(R.id.txtApeR);
         edtNom = findViewById(R.id.txtNomR);
         edtCon = findViewById(R.id.txtContraR);
         edtDni = findViewById(R.id.txtDniR);
         edtTel = findViewById(R.id.txtTelR);
-        edtFec = findViewById(txtFechaR);
+        edtFec = findViewById(R.id.txtFechaR);
 
         txtYaTienesCuenta = findViewById(R.id.txtYaTienesCuenta);
 
-        spnSexR=findViewById(R.id.spnSexR);
-        ArrayAdapter adapter=new ArrayAdapter<String>(this,
+        spnSexR = findViewById(R.id.spnSexR);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
                 android.R.layout.simple_list_item_1,
-                spnSexVal);
+                new String[]{"Hombre", "Mujer"});
         spnSexR.setAdapter(adapter);
 
         txtYaTienesCuenta.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent iLog = new Intent(RegistroPacientes.this, IniciarSesion.class);
+                Intent iLog = new Intent(RegistroPacientes.this, Principal.class);
                 startActivity(iLog);
             }
         });
 
-        btnRegistrar= findViewById(R.id.btnRegistrar);
+        btnRegistrar = findViewById(R.id.btnRegistrar);
 
-        //Funcion del boton Registrar
         btnRegistrar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                new DatabaseConnectionTask().execute();
+                String nombreUsuario = edtCor.getText().toString();
+                String contrasena = edtCon.getText().toString();
+                new DatabaseConnectionTask().execute(nombreUsuario, contrasena);
             }
         });
 
+        edtFec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mostrarSelector();
+            }
+        });
     }
 
-    //Clase Asincrona para las conexion y el registro de pacientes
-    private class DatabaseConnectionTask extends AsyncTask<Void, Void, Connection> {
-       protected Connection doInBackground(Void... voids) {
+    private void mostrarSelector() {
+        final Calendar calendario = Calendar.getInstance();
+        int aActual = calendario.get(Calendar.YEAR);
+        int mActual = calendario.get(Calendar.MONTH);
+        int dActual = calendario.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int a, int m, int d) {
+                        final String fecha = d + "/" + (m + 1) + "/" + a;
+                        edtFec.setText(fecha);
+                    }
+                }, aActual, mActual, dActual);
+        datePickerDialog.show();
+    }
+
+    private class DatabaseConnectionTask extends AsyncTask<String, Void, Integer> {
+        @Override
+        protected Integer doInBackground(String... params) {
+            String nombreUsuario = params[0];
+            String contrasena = params[1];
+
+            int resultado = -1;
+
             try {
                 Connection connection = DatabaseConnection.getConnection();
                 if (connection != null) {
                     String selectedGender = spnSexR.getSelectedItem().toString();
-                    // Crear una sentencia SQL INSERT
-                    String sql = "INSERT INTO pacientes (id_paciente, nombres, apellidos, correo, contrasena, telefono, fecha_nacimiento, sexo) VALUES (?, ?, ?, ?, ?, ?, to_date(?,'dd-MM-yyyy'), ?)";
+                    String sqlUsuario = "INSERT INTO usuarios (nombre_usuario, contrasena, id_rol) VALUES (?, ?, ?)";
+                    PreparedStatement pstUsuario = connection.prepareStatement(sqlUsuario, PreparedStatement.RETURN_GENERATED_KEYS);
+                    pstUsuario.setString(1, nombreUsuario);
+                    pstUsuario.setString(2, contrasena);
+                    pstUsuario.setInt(3, 3);
 
-                    // Crear un PreparedStatement
-                    PreparedStatement pst = connection.prepareStatement(sql);
+                    int rowsAffectedUsuario = pstUsuario.executeUpdate();
 
-                    // Establecer los valores para los parámetros
-                    pst.setString(1,edtDni.getText().toString());
-                    pst.setString(2,edtNom.getText().toString());
-                    pst.setString(3,edtApe.getText().toString());
-                    pst.setString(4,edtCor.getText().toString());
-                    pst.setString(5,edtCon.getText().toString());
-                    pst.setString(6,edtTel.getText().toString());
-                    pst.setString(7,edtFec.getText().toString());
-                    pst.setString(8,selectedGender);
+                    if (rowsAffectedUsuario > 0) {
+                        ResultSet generatedKeys = pstUsuario.getGeneratedKeys();
+                        int idUsuario = -1;
+                        if (generatedKeys.next()) {
+                            idUsuario = generatedKeys.getInt(1);
+                        }
 
-                    // Ejecutar la sentencia INSERT
-                    int rowsAffected = pst.executeUpdate();
+                        // Convertir la cadena de fecha en un objeto java.sql.Date
 
-                    // Cerrar la conexión y el PreparedStatement
-                    pst.close();
-                    connection.close();
 
-                    if (rowsAffected > 0) {
-                        return connection;
+                        String sqlPaciente = "INSERT INTO pacientes (id_paciente, nombres, apellidos, correo, contrasena, telefono, fecha_nacimiento, sexo, id_usuario) VALUES (?, ?, ?, ?, ?, ?,  to_date(?,'dd-MM-yyyy'), ?, ?)";
+                        PreparedStatement pstPaciente = connection.prepareStatement(sqlPaciente);
+                        pstPaciente.setString(1, edtDni.getText().toString());
+                        pstPaciente.setString(2, edtNom.getText().toString());
+                        pstPaciente.setString(3, edtApe.getText().toString());
+                        pstPaciente.setString(4, edtCor.getText().toString());
+                        pstPaciente.setString(5, edtCon.getText().toString());
+                        pstPaciente.setString(6, edtTel.getText().toString());
+                        pstPaciente.setString(7, edtFec.getText().toString());
+                        pstPaciente.setString(8, selectedGender);
+                        pstPaciente.setInt(9, idUsuario);
+
+                        int rowsAffectedPaciente = pstPaciente.executeUpdate();
+
+                        pstPaciente.close();
+
+                        if (rowsAffectedPaciente > 0) {
+                            resultado = idUsuario;
+                        }
                     }
+
+                    pstUsuario.close();
+
                 }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
 
-            return null;
+            return resultado;
         }
 
-    }
-
-    public void fechaNacimiento(View view){
-        edtFec.setOnClickListener(this::fechaNacimiento);
-        if (view.getId() == R.id.txtFechaR) {
-            mostrarSelector();
-        }
-    }
-
-    private void mostrarSelector() {
-        FragmentSelectorFecha nuevoFragmento = FragmentSelectorFecha.newInstance(new DatePickerDialog.OnDateSetListener() {
-            @Override
-            public void onDateSet(DatePicker datePicker, int a, int m , int d) {
-                Calendar calendario = Calendar.getInstance();
-                aActual = calendario.get(Calendar.YEAR);
-                mActual = calendario.get(Calendar.MONTH);
-                dActual = calendario.get(Calendar.DAY_OF_MONTH);
-
-
-                final String fecha = d + "/" + (m+1) + "/" + a;
-                fecA = a;
-                fecM = m;
-                fecD = d;
-                edtFec.setText(fecha);
+        @Override
+        protected void onPostExecute(Integer idUsuario) {
+            if (idUsuario != -1) {
+                mostrarToast("Usuario registrado exitosamente");
+            } else {
+                mostrarToast("No se pudo registrar el usuario");
             }
-        });
-        nuevoFragmento.show(getSupportFragmentManager(),"selector");
+        }
 
+        private void mostrarToast(String mensaje) {
+            Toast.makeText(RegistroPacientes.this, mensaje, Toast.LENGTH_SHORT).show();
+        }
     }
-
-
-
 }
